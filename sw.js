a71e964c295b717dcbb7cfceb28ba45d30e6fabf
Clarissa Colletta <122881121+MicolDellaColletta@users.noreply.github.com>
2026-03-22 20:37:35 +0000

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'farmaci-v1';
+const ASSETS = ['/farmaci-mamma/', '/farmaci-mamma/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
